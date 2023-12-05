7fd8795342f794a640e8d024ce30be030af69bac
pedrohcmoda <phcunha47@gmail.com>
2023-12-05 15:41:39 -0300

    atualizado para ext commands

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 # This example requires the 'message_content' privileged intents
 
-import discord
-import os
 import asyncio
+import os
+import discord
+from discord.ext import commands
 
-client = discord.Bot(intents=discord.Intents.all())
 
 
-@client.event
+bot = discord.Bot(intents=discord.Intents.all())
+
+
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     print("Hello, {member.id}")
     await member.dm_channel.send(f'Olá {member.name}, bem vindo ao server, manda teu nick ai kk!')
     try:
-        nickname = await client.wait_for('message', check=lambda message: message.author == member and message.channel == member.dm_channel, timeout=60)
+        nickname = await bot.wait_for('message', check=lambda message: message.author == member and message.channel == member.dm_channel, timeout=60)
     except asyncio.TimeoutError:
         await member.dm_channel.send(f'Você não respondeu a tempo. Tente novamente mais tarde.')
     else:
         await member.edit(nick=nickname.content)
         
-client.event(on_member_join)
+bot.event(on_member_join)
 
-
-client.run(os.environ["DISCORD_TOKEN"])
+bot.run(os.environ["DISCORD_TOKEN"])

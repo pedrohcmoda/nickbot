593d077747c0b9c00bee0baf0a6683541d09e856
pedrohcmoda <phcunha47@gmail.com>
2023-12-06 12:12:20 -0300

    atualizacao, bot funfando, alterado para .Client em vez de .Bot na criacao da variavel

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 # This example requires the 'message_content' privileged intents
 
 import asyncio
-import os
 import discord
-from discord.ext import commands
 
-
-
-bot = discord.Bot(intents=discord.Intents.all())
+bot = discord.Client(intents=discord.Intents.all())
 
 
 @bot.event
 async def on_member_join(member):
-    await member.create_dm()
-    print("Hello, {member.id}")
-    await member.dm_channel.send(f'Olá {member.name}, bem vindo ao server, manda teu nick ai kk!')
-    try:
-        nickname = await bot.wait_for('message', check=lambda message: message.author == member and message.channel == member.dm_channel, timeout=60)
-    except asyncio.TimeoutError:
-        await member.dm_channel.send(f'Você não respondeu a tempo. Tente novamente mais tarde.')
-    else:
-        await member.edit(nick=nickname.content)
-        
+  await member.create_dm()
+  print("Hello, {member.id}")
+  await member.dm_channel.send(
+      f'Olá {member.name}, bem vindo ao KOC, manda seu nick ingame ai')
+  try:
+    nickname = await bot.wait_for(
+        'message',
+        check=lambda message: message.author == member and message.channel ==
+        member.dm_channel,
+        timeout=60)
+  except asyncio.TimeoutError:
+    await member.dm_channel.send(
+        f'Você não respondeu a tempo, assim que possivel altere manualmente seu apelido no servidor, seu apelido precisa ser seu nick ingame'
+    )
+  else:
+    await member.edit(nick=nickname.content)
+
+
 bot.event(on_member_join)
 
-bot.run(os.environ["DISCORD_TOKEN"])
+bot.run("MTE4MTU5NjU3MTM4MTU1MTEzNQ.G5yUgK.AhKNJyg6HxQZ0ZtAI8F54oEkSgiElMkVrFpQCU")
---
+++ squarecloud.app
@@ -0,0 +1,5 @@
+DISPLAY_NAME=Nicker
+DESCRIPTION=Nicks
+MAIN=main.py
+MEMORY=128
+VERSION=recommended
